1990acd138338e7dc938b3a56220ca361e5f525c
eeennsu <diso592@naver.com>
2024-06-24 22:13:37 +0900

    fix: Fixed vercel deploy error

--- src/shared/utils/puppeteer.ts
+++ src/shared/utils/puppeteer.ts
@@ -6,7 +6,10 @@ export const getPageContentWithScroll = async (
     url: string,
     options: GoToOptions = { waitUntil: 'networkidle2' }
 ): Promise<string> => {
-    const browser = await puppeteer.launch()
+    const browser = await puppeteer.launch({
+        executablePath: process.env.CHROME_BINARY_PATH,
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    })
     const page = await browser.newPage()
 
     await page.goto(url, options)
@@ -23,7 +26,10 @@ export const getPageContent = async (
     url: string,
     options: GoToOptions = { waitUntil: 'networkidle2' }
 ): Promise<string> => {
-    const browser = await puppeteer.launch()
+    const browser = await puppeteer.launch({
+        executablePath: process.env.CHROME_BINARY_PATH,
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    })
     const page = await browser.newPage()
     await page.goto(url, options)
 
