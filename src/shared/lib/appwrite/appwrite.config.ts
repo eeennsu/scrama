68281804077526b7fc85199b9e30d102cfa46690
eeennsu <diso592@naver.com>
2024-12-29 11:58:30 +0900

    fix: Change favicon, remove unnecessary code

--- src/shared/lib/appwrite/appwrite.config.ts
+++
@@ -1,5 +0,0 @@
-import { checkEnvVariable } from '@/shared/utils'
-
-export const appwriteEndPoint = checkEnvVariable(process.env.APPWRITE_ENDPOINT)
-export const appwriteProject = checkEnvVariable(process.env.APPWRITE_PROJECT_ID)
-export const appwriteCreateAdminKey = checkEnvVariable(process.env.APPWRITE_CREATE_ADMIN_KEY)
